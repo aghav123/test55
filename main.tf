8f9b10359a2926b1ca2cf983b344a77b0a930439
aghav123 <109300798+aghav123@users.noreply.github.com>
2023-08-22 11:50:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,31 +17,3 @@ resource "aws_subnet" "subnet2" {
 
 
 
-resource "aws_subnet" "nat_gateway" {
-  availability_zone = data.aws_availability_zones.available.names[0]
-  cidr_block = "10.0.2.0/24"
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    "Name" = "DummySubnetNAT"
-  }
-}
-
-resource "aws_internet_gateway" "nat_gateway" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    "Name" = "DummyGateway"
-  }
-}
-
-resource "aws_route_table" "nat_gateway" {
-  vpc_id = aws_vpc.vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.nat_gateway.id
-  }
-}
-
-resource "aws_route_table_association" "nat_gateway" {
-  subnet_id = aws_subnet.nat_gateway.id
-  route_table_id = aws_route_table.nat_gateway.id
-}
