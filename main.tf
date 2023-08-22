48a2fa970d9c86f57b1e370d109170d4d0b1e59d
aghav123 <109300798+aghav123@users.noreply.github.com>
2023-08-22 14:04:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,11 @@
-resource "aws_subnet" "subnet2" {
-  depends_on = [
-    aws_vpc.custom,
-    aws_subnet.subnet1
-  ]
-  
-  vpc_id = "vpc-00bf0d10a6a41600c"
-
+resource "aws_subnet" "main" {
+  vpc_id     = "vpc-00bf0d10a6a41600c"
   cidr_block = "10.0.1.0/24"
-  
-  availability_zone = "ap-south-1"
-  
+
   tags = {
-    Name = "Private Subnet"
+    Name = "Main"
   }
 }
-
 
 
 
