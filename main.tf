e626aa56194511830e1028994aa432aab6489614
aghav123 <109300798+aghav123@users.noreply.github.com>
2023-08-22 15:01:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_route_table" "rt" {
   vpc_id     = "nat-0d688bbff8a47b274"
   route {
 
-        cidr_block = "0.0.0.0/0"
+        cidr_block = "10.0.120.0/24"
         gateway_id = "vpc-00bf0d10a6a41600c"
     }
 
