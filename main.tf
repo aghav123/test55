3c0472ab74f5d383ccd0b4e82f39a6f35bb0ed15
aghav123 <109300798+aghav123@users.noreply.github.com>
2023-08-22 16:30:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,3 +36,25 @@ resource "aws_security_group" "demo" {
     cidr_blocks = ["10.0.120.0/24"]
   }
 }
+
+resource "aws_lambda_function" "demolambda" {
+  function_name    = "demo lambda"
+  filename         = "lambda_function_payload.zip"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+  handler          = "index.handler"
+  role             = DevOps-Candidate-Lambda-Role
+  runtime          = "nodejs14.x"
+  vpc_config {
+    subnet_ids = [000494df60610685b]
+    security_group_ids = [07c6cdde377e486d0]
+  }
+}
+
+
+
+
+
+
+
+
+
