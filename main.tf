26193723ee2a2c8e3e3e6daa666be6200f393df1
aghav123 <109300798+aghav123@users.noreply.github.com>
2023-08-22 14:35:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,20 @@ resource "aws_subnet" "main" {
 
 
 
+resource "aws_route_table" "example" {
+   vpc_id     = "vpc-00bf0d10a6a41600c"
+
+  route {
+    cidr_block = "10.0.120.0/24"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
