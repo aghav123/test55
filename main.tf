2c411722f85f6f95a06b0e4b12db67801b4288c8
aghav123 <109300798+aghav123@users.noreply.github.com>
2023-08-22 12:56:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,34 @@ resource "aws_subnet" "subnet2" {
 
 
 
+resource "aws_route_table" "example" {
+  vpc_id = "vpc-00bf0d10a6a41600c"
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+   
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+
+# Creating Route table for Private Subnet
+resource "aws_route_table" "rt_private" {
+    vpc_id = aws_vpc.my_vpc.id
+tags = {
+        Name = "Route Table for the Private Subnet"
+    }
+}
+resource "aws_route_table_association" "rt_associate_private_2" {
+    subnet_id = aws_subnet.demosubnet2.id
+    route_table_id = aws_route_table.rt_private.id
+}
+
