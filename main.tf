67e4b8043b135685d6bf6130d3ed7d297cf5728d
aghav123 <109300798+aghav123@users.noreply.github.com>
2023-08-22 15:04:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,22 +7,14 @@ resource "aws_subnet" "main" {
   }
 }
 
-#creating route table
-resource "aws_route_table" "rt" {
+#creating internet gateway for our subnets to be able to connect to internet
+resource "aws_internet_gateway" "igw" {
 
-  vpc_id     = "nat-0d688bbff8a47b274"
-  route {
-
-        cidr_block = "10.0.120.0/24"
-        gateway_id = "vpc-00bf0d10a6a41600c"
-    }
-
+   
+    vpc_id     = "vpc-00bf0d10a6a41600c"
 
     tags = {
 
-        Name = "My-Private-Routing-Table"
+        Name = "My-IGW"
     }
-
 }
-
-
